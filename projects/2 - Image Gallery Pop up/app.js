c3d9c3b91678c470802c74686247d30c670e7289
Brian Bycynski <high5bri@hotmail.com>
2021-04-29 11:23:15 -0400

    finished image gallery

---
+++ projects/2 - Image Gallery Pop up/app.js
@@ -0,0 +1,25 @@
+//
+//
+//
+
+const fullImage = document.querySelector('.full-img');
+const smallImage = document.querySelectorAll('.gallery img');
+
+const modal = document.querySelector('.modal');
+
+smallImage.forEach(function (img) {
+  img.addEventListener('click', function () {
+    modal.classList.add('open');
+    fullImage.classList.add('open');
+
+    const originalQuality = img.getAttribute('alt');
+    fullImage.src = `img/full/${originalQuality}.jpg  `;
+  });
+});
+
+modal.addEventListener('click', function (e) {
+  if (e.target.classList.contains('modal')) {
+    modal.classList.remove('open');
+    fullImage.classList.remove('open');
+  }
+});
