718cebb76efe707ef9723a553958324ad37a54c2
Brian Bycynski <high5bri@hotmail.com>
2021-04-30 02:52:08 -0400

    Finished Image Gallery Pop Up Project

--- projects/1 - Modal/app.js
+++ projects/1 - Modal/app.js
@@ -1,10 +1,10 @@
 //
 //
 
-const openBtn = document.querySelector('#open');
-const modalContainer = document.querySelector('#modal-container');
+const openBtn = document.querySelector('.open');
+const modalContainer = document.querySelector('.modal-container');
 
-const closeBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('.modal-btn');
 
 openBtn.addEventListener('click', () => {
   modalContainer.classList.add('show');
