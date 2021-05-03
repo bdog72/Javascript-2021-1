8a11976112f775a609ef7f54e033e03c719731f1
Brian Bycynski <high5bri@hotmail.com>
2021-05-03 02:33:23 -0400

    finished keyboards app

---
+++ projects/12 - The Keyboard App/app.js
@@ -0,0 +1,16 @@
+//
+//
+
+const keyContainers = document.querySelectorAll('.keys');
+let textArea = document.querySelector('.display textarea');
+
+keyContainers.forEach(function (key) {
+  key.addEventListener('click', function (e) {
+    // console.log(e.path[0].value.toLowerCase());
+
+    if (!e.path[0].value) return;
+
+    let keyClicked = e.path[0].value.toLowerCase();
+    textArea.value += keyClicked;
+  });
+});
