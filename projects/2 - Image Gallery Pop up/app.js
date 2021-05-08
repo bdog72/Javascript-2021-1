bfaf55513bba6a9e3b3bd20d5da06100ac820961
Brian Bycynski <high5bri@hotmail.com>
2021-05-08 02:34:21 -0400

    Finished Cursor Effect

--- projects/2 - Image Gallery Pop up/app.js
+++ projects/2 - Image Gallery Pop up/app.js
@@ -1,2 +1,25 @@
 //
 //
+
+const fullImg = document.querySelector('.full-img');
+const smallImg = document.querySelectorAll('.gallery img');
+
+const modal = document.querySelector('.modal');
+
+smallImg.forEach((img) => {
+  img.addEventListener('click', () => {
+    modal.classList.add('open');
+    fullImg.classList.add('open');
+
+    // Changing the images dynamically
+    const originalQuality = img.getAttribute('alt');
+    fullImg.src = `img/full/${originalQuality}.jpg`;
+  });
+});
+
+modal.addEventListener('click', function (e) {
+  if (e.target.classList.contains('modal')) {
+    modal.classList.remove('open');
+    fullImg.classList.remove('open');
+  }
+});
