40956a21a49592e27b8c1d374845371d2a20b3d0
Brian Bycynski <high5bri@hotmail.com>
2021-05-18 02:13:35 -0400

    Finished Text Reader App

--- projects/20 - The Text Reader App/app.js
+++ projects/20 - The Text Reader App/app.js
@@ -1,2 +1,64 @@
 //
 //
+
+const textDisplay = document.querySelector('#text');
+const speedBtn = document.querySelector('#speed');
+
+const readBtn = document.querySelector('.read');
+const pauseBtn = document.querySelector('.pause');
+
+const stopBtn = document.querySelector('.stop');
+
+let currentChar;
+
+// Readding functionality
+readBtn.addEventListener('click', function () {
+  readText(textDisplay.value);
+});
+
+// Pausing functionality
+pauseBtn.addEventListener('click', pauseText);
+
+// Stoping functionality
+stopBtn.addEventListener('click', stopText);
+
+// Speed Input functionality
+speedBtn.addEventListener('input', function () {
+  stopText();
+  readText(utterance.text.substr(currentChar));
+});
+
+const utterance = new SpeechSynthesisUtterance();
+utterance.addEventListener('end', function () {
+  textDisplay.disabled = false;
+});
+
+utterance.addEventListener('boundary', function (e) {
+  currentChar = e.charIndex;
+});
+
+// Read text function
+function readText(testText) {
+  if (speechSynthesis.paused && speechSynthesis.speaking) {
+    return speechSynthesis.resume();
+  }
+
+  // console.log(utterance);
+  utterance.text = testText;
+  utterance.rate = speedBtn.value || 1;
+  textDisplay.disabled = true;
+  speechSynthesis.speak(utterance);
+}
+
+// Paus Text function
+function pauseText() {
+  if (speechSynthesis.speaking) {
+    speechSynthesis.pause();
+  }
+}
+
+// Stop text function
+function stopText() {
+  speechSynthesis.resume();
+  speechSynthesis.cancel();
+}
